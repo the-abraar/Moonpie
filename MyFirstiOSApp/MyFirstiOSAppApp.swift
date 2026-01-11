78722220658732a8f137537fe935a7aceee542f6
Abraar <the.abraar.rar@gmail.com>
2026-01-11 11:52:01 +0600

    Initial Commit

---
+++ MyFirstiOSApp/MyFirstiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstiOSAppApp.swift
+//  MyFirstiOSApp
+//
+//  Created by Abraar Masud Nafiz on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
