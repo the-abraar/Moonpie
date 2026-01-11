b1cd37cf337279024a24ca50f653ceed18a34fa3
Abraar <the.abraar.rar@gmail.com>
2026-01-11 12:03:47 +0600

    text display

--- MyFirstiOSApp/ContentView.swift
+++ MyFirstiOSApp/ContentView.swift
@@ -5,19 +5,53 @@
 //  Created by Abraar Masud Nafiz on 1/11/26.
 //
 
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+//
+//#Preview {
+//    ContentView()
+//}
+//struct LandmarksApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
+
+
 import SwiftUI
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("MoonPie")
+                .font(.title)
+            HStack {
+                Text("The Royelest of all Royal Cats")
+                    .font(.subheadline)
+                Spacer()
+                Text("Sylhet")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
