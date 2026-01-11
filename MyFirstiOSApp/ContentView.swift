2ce5e640f3f2584ae457b2409e62467e3cb20c80
Abraar <the.abraar.rar@gmail.com>
2026-01-11 12:41:00 +0600

    Final profile

--- MyFirstiOSApp/ContentView.swift
+++ MyFirstiOSApp/ContentView.swift
@@ -5,50 +5,47 @@
 //  Created by Abraar Masud Nafiz on 1/11/26.
 //
 
-//import SwiftUI
-//
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-//    }
-//}
-//
-//#Preview {
-//    ContentView()
-//}
-//struct LandmarksApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//    }
-//}
-
-
-
 import SwiftUI
 
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("MoonPie")
-                .font(.title)
-            HStack {
-                Text("The Royelest of all Royal Cats")
-                    .font(.subheadline)
-                Spacer()
-                Text("Sylhet")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 280)
+
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+
+            VStack(alignment: .leading) {
+                Text("Moonpie")
+                    .font(.title)
+                HStack {
+                    Text("The Royalest of all Royal Cats")
+                    Spacer()
+                    Text("Persian")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
+
+                Divider()
+
+
+                Text("About this Meaw")
+                    .font(.title2)
+                Text("Location : Sylhet, Bangladesh")
+                Text("Age : 1 year 2 months")
+                Text("Looking for a loving cat companion.")
             }
+            .padding()
+
+
+            Spacer()
         }
-        .padding()
     }
 }
 
